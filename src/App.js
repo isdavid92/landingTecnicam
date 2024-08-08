c0f48d06d800572c2231b8a008f1c08970e5c365
ISRAEL AGUILAR <117057916+isdavid92@users.noreply.github.com>
2024-08-07 21:40:33 -0500

    al 80% casi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./Assets/Img/logo.png";
+import tool from "./Assets/Img/tool.png";
+import sPss from "./Assets/Img/sPss.png";
+import gmail from "./Assets/Img/gmail.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <div className="title1">
+          <h1 className="title1h1">CÁMARAS</h1>
+        </div>
+        <div className="title2">
+          <h1 className="title2h1">SEGURIDAD</h1>
+        </div>
+      </div>
+
+      <div className="titleSub">
+        <div className="titleServicio">
+          <img src={tool} alt="tool" className="tool" />
+          <h1 className="titleServicioh1">Servicio Técnico</h1>
+        </div>
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+
+      <div className="divServicios">
+        <img src={sPss} alt="sPss" className={"sPss"} />
+        <ul className="servicios">
+          <li className="servicio">Venta, instalación y configuración de cámaras de seguridad</li>
+          <li className="servicio">Mantenimientos preventivos y correctivos</li>
+          <li className="servicio">Asesorías y acompañamientos</li>
+          <li className="servicio">Instalación y configuración de redes locales de
+            internet (router, repetidor o switch)</li>
+        </ul>
+      </div>
+
+      <div className="divResumenCorreo">
+        <div className="resumen">
+          <h3 className="resumenh3">
+            Para la casa, el apartamento, la oficina, la finca, la empresa, la bodega... seguridad electrónica que le permite monitorear sus espacios a través de internet.
+          </h3>
+        </div>
+        <div className="correo">
+          <a href="mailto:tecnicamcctv@gmail.com" className="aGmail">
+            <img src={gmail} alt="gmail" className="gmail" />
+          </a>
+          <a className="correoh1" href="mailto:tecnicamcctv@gmail.com">tecnicamcctv@gmail.com</a>
+        </div>
+      </div>
+
+      <div className="contactos">
+        <div className="contacto1">
+        </div>
+        <div className="contacto2">
+        </div>
+      </div>
+
+      <div className="pie">
+        <div className="pie2">
+          <h1 className="pie2h1">ISRAEL AGUILAR</h1>
+        </div>
+        <div className="pie1">
+          <h1 className="pie1h1">TÉCNICO</h1>
+        </div>
+      </div>
     </div>
   );
 }
